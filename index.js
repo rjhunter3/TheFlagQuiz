cdad91785183f4cd9796320bc5e3e19e6cf3f861
rjhunter3 <robertjh30@gmail.com>
2019-03-28 13:26:38 +0000

    Worked on changing page content
    Done research, Replaced list on navbar with clickable buttons, initialised client JS, still need to populate content and get server stuff going (should enable content to be drawn from country api)

---
+++ index.js
@@ -0,0 +1,16 @@
+
+document.getElementById('new').addEventListener('click', async function(event){
+    let response = ['response'];
+    document.getElementById('content').innerHTML = response;
+
+});
+
+/*$('.nav navbar-nav navbar-right').on('click', 'a', function(e){
+  
+    // 'this' is the clicked anchor
+    
+    var text = this.text;
+    var href = this.href;
+    
+    alert(text);
+});*/
