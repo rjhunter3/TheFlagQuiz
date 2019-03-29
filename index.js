48c8a02c92b94a2bf2a73c5b06313804428156d2
rjhunter3 <robertjh30@gmail.com>
2019-03-29 14:38:08 +0000

    Added home link in logo
    Through get element by ID and inner HTML. Will look at server initialisation

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ document.getElementById('new').addEventListener('click', async function(event){
     document.getElementById('content').innerHTML = response;
 
 });
+document.getElementById('home').addEventListener('click', async function(event){
+    let response = '<div class="jumbotron">';
+    response += '<h1>Flag Quiz</h1>';
+    response += '<p>Test your vexillological knowledge!</p>';
+    response += '</div>';
+    response += '<div class="wrapper">';
+    response += '<div class="sliding-background"></div>';
+    response += '</div>';
+    document.getElementById('content').innerHTML = response;
+})
+
 
 /*$('.nav navbar-nav navbar-right').on('click', 'a', function(e){
   
